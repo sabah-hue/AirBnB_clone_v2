1254e20ff5b4eeb9e982810a202d0e92ea10f90e
sabah-hue <sabah.abdelbaset@gmail.com>
2024-05-04 04:23:30 +0300

    puppet config

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,42 +1,13 @@
 # Using Puppet, sets up web servers for the deployment of web_static
-package { 'nginx':
-  ensure   => 'present',
-  provider => 'apt'
-}
-
-file { '/data':
-    ensure  => 'directory'
-}
-
-file { '/data/web_static':
-    ensure => 'directory'
-}
-
-file { '/data/web_static/releases':
-    ensure => 'directory'
-}
-
-file { '/data/web_static/releases/test':
-    ensure => 'directory'
-}
-
-file { '/data/web_static/shared':
-    ensure => 'directory'
-}
-
-file { '/data/web_static/releases/test/index.html':
-    ensure  => 'present',
-    content => 'Holberton School'
-}
-
-file { '/data/web_static/current':
-    ensure => 'link',
-    target => '/data/web_static/releases/test'
-}
-
 exec { 'install_nginx':
-    command  => 'sudo chown -R ubuntu:ubuntu /data/;
-    sudo sed -i '50 i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}' /etc/nginx/sites-enabled/default;
+    command  => 'sudo apt-get -y update;
+    sudo apt-get -y install nginx;
+    sudo mkdir -p /data/web_static/releases/test/;
+    sudo echo "Holberton School" > /data/web_static/releases/test/index.html;
+    sudo mkdir -p /data/web_static/shared/;
+    sudo ln -sf /data/web_static/releases/test/ /data/web_static/current;
+    sudo chown -R ubuntu:ubuntu /data/;
+    sudo sed -i "50 i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}" /etc/nginx/sites-enabled/default;
     sudo service nginx restart',
     provider => shell,
 }
