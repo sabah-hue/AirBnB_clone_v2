b58b5da96a645efdd4633d1c10a64a1c481148b8
sabah-hue <sabah.abdelbaset@gmail.com>
2024-05-04 14:39:34 +0300

    output issue in puppet

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,7 +3,6 @@
 generates a .tgz archive from the contents of the web_static
 """
 
-
 def do_pack():
     """ generates a .tgz archive """
     
--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -26,16 +26,18 @@ file { '/data/web_static/shared':
 
 file { '/data/web_static/releases/test/index.html':
     ensure  => 'present',
-    content => 'Holberton School'
-}
-
-file { '/data/web_static/current':
-    ensure => 'link',
-    target => '/data/web_static/releases/test'
+    content => '<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>'
 }
 
 exec { 'install_nginx':
-    command  => 'sudo chown -R ubuntu:ubuntu /data/;
+    command  => 'ln -sf /data/web_static/releases/test/ /data/web_static/current;
+    sudo chown -R ubuntu:ubuntu /data/;
     sed -i "50 i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}" /etc/nginx/sites-enabled/default;
     sudo service nginx restart',
     provider => shell,
