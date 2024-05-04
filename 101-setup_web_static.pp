ef02fcaebacf545b493588a15edf2804bad6ff62
sabah-hue <sabah.abdelbaset@gmail.com>
2024-05-04 03:40:05 +0300

    puppet file

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,9 +1,9 @@
 # Using Puppet, sets up web servers for the deployment of web_static
-exec { 'deploy_server':
+exec { 'install_nginx':
     command  => 'sudo apt-get -y update;
     sudo apt-get -y install nginx;
     sudo mkdir -p /data/web_static/releases/test/;
-    sudo echo "<html><head></head><body>Holberton School</body></html>" > /data/web_static/releases/test/index.html;
+    sudo echo "Holberton School" > /data/web_static/releases/test/index.html;
     sudo mkdir -p /data/web_static/shared/;
     sudo ln -sf /data/web_static/releases/test/ /data/web_static/current;
     sudo chown -R ubuntu:ubuntu /data/;
