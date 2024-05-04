965455faf73c6452c88f5a178228cd2baf9b0067
sabah-hue <sabah.abdelbaset@gmail.com>
2024-05-04 15:05:49 +0300

    puppet issue

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,7 +2,7 @@
 """ 
 generates a .tgz archive from the contents of the web_static
 """
-
+from fabric import 
 def do_pack():
     """ generates a .tgz archive """
     
--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,42 +1,17 @@
 # Using Puppet, sets up web servers for the deployment of web_static
-package { 'nginx':
-    ensure   => 'present',
-    provider => 'apt',
-}
-
-file { '/data':
-    ensure  => 'directory'
-}
-
-file { '/data/web_static':
-    ensure => 'directory'
-}
-
-file { '/data/web_static/releases':
-    ensure => 'directory'
-}
-
-file { '/data/web_static/releases/test':
-    ensure => 'directory'
-}
-
-file { '/data/web_static/shared':
-    ensure => 'directory'
-}
-
-file { '/data/web_static/releases/test/index.html':
-    ensure  => 'present',
-    content => '<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>'
-}
-
 exec { 'install_nginx':
-    command  => 'ln -sf /data/web_static/releases/test/ /data/web_static/current;
+    command  => 'sudo apt-get -y update;
+    sudo apt-get -y install nginx;
+    sudo mkdir -p /data/web_static/releases/test/;
+    sudo echo "<html>
+    <head>
+    </head>
+    <body>
+        Holberton School
+    </body>
+    </html>" > /data/web_static/releases/test/index.html;
+    sudo mkdir -p /data/web_static/shared/;
+    sudo ln -sf /data/web_static/releases/test/ /data/web_static/current;
     sudo chown -R ubuntu:ubuntu /data/;
     sed -i "50 i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}" /etc/nginx/sites-enabled/default;
     sudo service nginx restart',
