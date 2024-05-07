13028bb136dcceb98fd1cd569e6984cbca47e014
sabah-hue <sabah.abdelbaset@gmail.com>
2024-05-07 05:07:07 +0300

    doc issue

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,13 +9,15 @@ from datetime import datetime
 
 def do_pack():
     """ generates a .tgz archive """
-    servers = ["100.25.41.212", "100.25.103.239"]
-    for server in servers:
-        d_form = datetime.now().strftime("%Y%m%d%H%M%S")
-        with Connection(host=server, user="ubuntu",
-                        connect_kwargs={
-                            "key_filename": "/root/.ssh/id_rsa"}) as c:
-            c.local('mkdir -p versions')
-            x = c.local("tar -czvf versions/web_static_{}.tgz web_static/"
-                        .format(d_form))
-            return x
+    d_form = datetime.now().strftime("%Y%m%d%H%M%S")
+    with Connection(host="100.25.41.212", user="ubuntu",
+                    connect_kwargs={
+                        "key_filename": "/root/.ssh/id_rsa"}) as c:
+        c.local('mkdir -p versions')
+        x = c.local(f"tar -czvf versions/web_static_{d_form}.tgz web_static/")
+        return x
+
+
+if __name__ == "__main__":
+    """run in terminal"""
+    do_pack()
