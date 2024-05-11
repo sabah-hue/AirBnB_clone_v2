b54ab5cb615d235eae1bb337ca5cb9e3feafd20e
sabah-hue <sabah.abdelbaset@gmail.com>
2024-05-11 03:20:24 +0300

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """distributes an archive to your web servers, using the function do_deploy"""
+from fabric.connection import Connection
 from fabric.api import put, run, env
 from os import path
-
 
 env.user = "ubuntu"
 env.my_ssh_private_key = "/root/.ssh/id_rsa"
@@ -11,19 +11,25 @@ env.hosts = ["100.25.41.212", "100.25.103.239"]
 
 def do_deploy(archive_path):
     """distributes an archive to my web servers"""
-    if not (path.exists(archive_path)):
-        return false
+    if not path.exists(archive_path):
+        return False
     n_folder = archive_path.split("/")[-1].split(".")[0]
+    release = "/data/web_static/releases"
     for host in env.hosts:
-        put(archive_path, '/tmp/')
-        run(f"sudo mkdir -p /data/web_static/releases/{n_folder}")
-        run(f"sudo tar -xzf /tmp/{n_folder}.tgz -C \
-            /data/web_static/releases/web_static_{n_folder}/")
-        run(f"sudo rm /tmp/web_static_{n_folder}.tgz")
-        run(f"sudo mv /data/web_static/releases/ \
-            {n_folder}/web_static/* /data/web_static/releases/{n_folder}/")
-        run(f"sudo rm -rf /data/web_static/releases/ {n_folder}/web_static")
-        run("sudo rm -rf /data/web_static/current")
-        run(f"ln -s /data/web_static/releases/{n_folder}/ \
-            /data/web_static/current")
-        print("New version deployed!")
+        with Connection(host=host,
+                        user=env.user,
+                        connect_kwargs={
+                            "key_filename": env.my_ssh_private_key,
+                        },
+                        ) as c:
+            c.put(archive_path, '/tmp/')
+            c.run(f"sudo mkdir -p {release}/{n_folder}")
+            c.run(f"sudo tar -xzf /tmp/{n_folder}.tgz -C \
+                  {release}/web_static_{n_folder}/")
+            c.run(f"sudo rm /tmp/web_static_{n_folder}.tgz")
+            c.run(f"sudo mv {release}/{n_folder}/web_static/* \
+                  {release}/{n_folder}/")
+            c.run(f"sudo rm -rf {release}/{n_folder}/web_static")
+            c.run("sudo rm -rf /data/web_static/current")
+            c.run(f"ln -s {release}/{n_folder}/ /data/web_static/current")
+            print("New version deployed!")
