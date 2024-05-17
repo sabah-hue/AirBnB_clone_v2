1965d203cd139cfb90347b9f54609bce95bfe505
sabah-hue <sabah.abdelbaset@gmail.com>
2024-05-17 11:47:34 +0300

    hbnb route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""start flask app"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_bnb():
+    """say hello"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def bnb_page():
+    """say hello"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    """run in cmd"""
+    app.run(host='0.0.0.0', port=5000)
