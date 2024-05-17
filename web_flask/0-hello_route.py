52358f65b6c5d4bfbad6a27f657269cc19b47992
sabah-hue <sabah.abdelbaset@gmail.com>
2024-05-17 11:40:11 +0300

    style issue

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,10 +7,12 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello_bnb():
     """say hello"""
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     """run in cmd"""
