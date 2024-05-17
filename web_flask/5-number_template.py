af9b3ff68eb95487282f3c8ad87d5626919e4358
sabah-hue <sabah.abdelbaset@gmail.com>
2024-05-17 13:24:38 +0300

    render html page

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""start flask app"""
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='../templates')
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_bnb():
+    """say hello"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def bnb_page():
+    """say HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def hello_c(text):
+    """display text"""
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def hello_python(text):
+    """display text"""
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route('/number/<int:n>')
+def display_number(n):
+    """Display number"""
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>')
+def display_html_page(n):
+    """Display number inside html page"""
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == '__main__':
+    """run in cmd"""
+    app.run(host='0.0.0.0', port=5000)
