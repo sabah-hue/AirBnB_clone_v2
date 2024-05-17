f3a478c394db82d30977d5806115dea87c84dc2d
sabah-hue <sabah.abdelbaset@gmail.com>
2024-05-17 13:30:29 +0300

    output issue

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -44,7 +44,7 @@ def display_number(n):
 @app.route('/number_template/<int:n>')
 def display_html_page(n):
     """Display number inside html page"""
-    return render_template('5-number.html', n=n)
+    return render_template('5-number.html', number=n)
 
 
 if __name__ == '__main__':
