9da9673a198e16398553fa88fff345b1c9d3ea6d
sabah-hue <sabah.abdelbaset@gmail.com>
2024-05-19 08:30:39 +0300

    list cities

--- models/place.py
+++ models/place.py
@@ -9,30 +9,51 @@ from sqlalchemy.orm import relationship
 place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60),
                              ForeignKey('places.id'),
+                             primary_key=True,
                              nullable=False),
                       Column('amenity_id', String(60),
                              ForeignKey('amenities.id'),
-                             nullable=False)
+                             primary_key=True, nullable=False)
                       )
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    # city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    # user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    # name = Column(String(128), nullable=False)
+    # description = Column(String(1024), nullable=True)
+    # number_rooms = Column(Integer, default=0, nullable=False)
+    # number_bathrooms = Column(Integer, default=0, nullable=False)
+    # max_guest = Column(Integer, default=0, nullable=False)
+    # price_by_night = Column(Integer, default=0, nullable=False)
+    # latitude = Column(Float, nullable=True)
+    # longitude = Column(Float, nullable=True)
+    # amenity_ids = []
+    # reviews = relationship('Review', cascade='all,delete', backref='place')
+    # amenities = relationship('Amenity', secondary='place_amenity',
+    #                              backref='places', viewonly=False)
+    city_id = Column(String(60), ForeignKey("cities.id",
+                     ondelete='CASCADE'), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id",
+                     ondelete='CASCADE'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, default=0, nullable=False)
-    number_bathrooms = Column(Integer, default=0, nullable=False)
-    max_guest = Column(Integer, default=0, nullable=False)
-    price_by_night = Column(Integer, default=0, nullable=False)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
-    reviews = relationship('Review', cascade='all,delete', backref='place')
-    amenities = relationship('Amenity', secondary='place_amenity',
-                                 backref='places', viewonly=False)
+    reviews = relationship('Review', backref='place',
+                           cascade='all, delete-orphan',
+                           passive_deletes=True)
+    amenities = relationship('Amenity', backref='place_amenities',
+                             cascade='all, delete',
+                             secondary=place_amenity,
+                             viewonly=False,
+                             passive_deletes=True)    
 
     @property
     def reviews(self):
---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""start flask app"""
+
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.teardown_appcontext
+def teardown_app(exception=None):
+    """ remove session """
+    if storage is not None:
+        storage.close()
+
+
+@app.route('/cities_by_states')
+def display_states():
+    """Display states inside html page"""
+    states = storage.all('State').values()
+    cities = storage.all('City').values()
+    return render_template('7-states_list.html', states=states, cities=cities)
+
+
+if __name__ == '__main__':
+    """run in cmd"""
+    app.run(host='0.0.0.0', port=5000)
