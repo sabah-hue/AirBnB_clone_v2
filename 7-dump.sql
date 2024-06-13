268c413421776f396cde325ceffd04d058173ac7
sabah-hue <sabah.abdelbaset@gmail.com>
2024-06-13 07:32:20 +0300

    display states and cities

--- 7-dump.sql
+++ 7-dump.sql
@@ -16,36 +16,36 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 -- Drop database
-DROP DATABASE IF EXISTS hbnb_dev_db;
+-- DROP DATABASE IF EXISTS hbnb_dev_db;
 
--- Create database + user if doesn't exist
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
-GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
+-- -- Create database + user if doesn't exist
+-- CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+-- SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+-- GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- FLUSH PRIVILEGES;
 
-USE hbnb_dev_db;
+-- USE hbnb_dev_db;
 
 --
 -- Table structure for table `cities`
 --
 
-DROP TABLE IF EXISTS `cities`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
+-- DROP TABLE IF EXISTS `cities`;
+-- /*!40101 SET @saved_cs_client     = @@character_set_client */;
+-- /*!40101 SET character_set_client = utf8 */;
+-- CREATE TABLE `cities` (
+--   `id` varchar(60) NOT NULL,
+--   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+--   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+--   `name` varchar(128) NOT NULL,
+--   `state_id` varchar(60) NOT NULL,
+--   PRIMARY KEY (`id`),
+--   KEY `state_id` (`state_id`),
+--   CONSTRAINT `cities_ibfk_1` FOREIGN KEY (`state_id`) REFERENCES `states` (`id`)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `cities` (
-  `id` varchar(60) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `name` varchar(128) NOT NULL,
-  `state_id` varchar(60) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `state_id` (`state_id`),
-  CONSTRAINT `cities_ibfk_1` FOREIGN KEY (`state_id`) REFERENCES `states` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `cities`
@@ -61,17 +61,17 @@ UNLOCK TABLES;
 -- Table structure for table `states`
 --
 
-DROP TABLE IF EXISTS `states`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
+-- DROP TABLE IF EXISTS `states`;
+-- /*!40101 SET @saved_cs_client     = @@character_set_client */;
+-- /*!40101 SET character_set_client = utf8 */;
+-- CREATE TABLE `states` (
+--   `id` varchar(60) NOT NULL,
+--   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+--   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+--   `name` varchar(128) NOT NULL,
+--   PRIMARY KEY (`id`)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `states` (
-  `id` varchar(60) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `name` varchar(128) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `states`
